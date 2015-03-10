218d385df39ca94d7b5c8d6ed94ea66e0d3a86fa
cyzanfar <cghazanfar10@gmail.com>
2015-03-10 17:28:45 -0400

    done

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,6 @@
 class PetsController < ApplicationController
 
-  # before_action :set_pet, only: [:show, :edit, :update, :destroy]
   PETFINDER = Petfinder::Client.new('bf43919be64d0ffed94aa9fa2acfd9d0', '11f2621e8fac20123e25a1807f0e61fe')
-  # GET /pets
-  # GET /pets.json
-
 
   def search
     @pet = Pet.new
@@ -15,31 +11,34 @@ class PetsController < ApplicationController
   end
 
   def results
-    # destroy_shelters
+
     @animals = Pet.all
     @animals.collect do |animal|
       animal.shelter_name = Shelter.find_or_create_by(name: PETFINDER.shelter(animal.shelter_id).name, email: PETFINDER.shelter(animal.shelter_id).email, phone: PETFINDER.shelter(animal.shelter_id).phone, city: PETFINDER.shelter(animal.shelter_id).city, zip: PETFINDER.shelter(animal.shelter_id).zip).name
 
       animal.save
+
     end
-     # binding.pry
+
   end
 
   def destroy_pets
     Pet.destroy_all
   end
-
-  def create
+   t5 = Time.now
+   def create
     # binding.pry
     # destroy_pets
      @petfinders = PETFINDER.find_pets(pet_finder_type, pet_finder_zip, clean_params)
       # binding.pry
-      @petfinders.each do |petfinder|
-        PETFINDER.shelter(shelter_id: petfinder.shelter_id).each do |shelter|
-          Shelter.find_or_create_by(name: shelter.name, phone: shelter.phone, city: shelter.city, zip: shelter.zip)
-        end
-        @desired_pet = Pet.find_or_create_by(name: petfinder.name, age: petfinder.age, sex: petfinder.sex, type_name: petfinder.animal, description: petfinder.description, shelter_code: petfinder.shelter_id, shelter_id: shelter.id)
-      end
+
+      # # @petfinders.each do |petfinder|
+      # #   PETFINDER.shelter(shelter_id: petfinder.shelter_id).each do |shelter|
+      # #     Shelter.find_or_create_by(name: shelter.name, phone: shelter.phone, city: shelter.city, zip: shelter.zip)
+
+      # #   end
+      # #   @desired_pet = Pet.find_or_create_by(name: petfinder.name, age: petfinder.age, sex: petfinder.sex, type_name: petfinder.animal, description: petfinder.description, shelter_code: petfinder.shelter_id, shelter_id: shelter.id)
+      # end
       @desired_pet.age = petfinder.age
       @desired_pet.size = petfinder.size
       @desired_pet.sex = petfinder.sex
@@ -54,9 +53,9 @@ class PetsController < ApplicationController
       # binding.pry
       redirect_to :controller=>'pets', :action => 'results'
   end
-
-
-
+      t6 = Time.now
+      timer3 = t6-t5
+      binding.pry
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def clean_params
