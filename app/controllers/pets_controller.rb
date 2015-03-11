1f2d84e1247fe3cb382cfd0e1595f067b1d4a8d7
Cyrus Ghazanfar <cghazanfar10@gmail.com>
2015-03-11 09:22:32 -0400

    Update pets_controller.rb

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,6 @@ class PetsController < ApplicationController
 
   def search
     @pets = Pet.where(clean_params)
-    # binding.pry
     render :results
   end
 
@@ -18,51 +17,11 @@ class PetsController < ApplicationController
 
   def thinned_params
     params["pets"].delete_if {|k, v| v.empty?}
-
   end
-
-  def destroy_shelters
-    Shelter.destroy_all
-  end
-
-  def results
-
-    @animals = Pet.all
-    @animals.collect do |animal|
-      animal.shelter_name = Shelter.find_or_create_by(name: PETFINDER.shelter(animal.shelter_id).name, email: PETFINDER.shelter(animal.shelter_id).email, phone: PETFINDER.shelter(animal.shelter_id).phone, city: PETFINDER.shelter(animal.shelter_id).city, zip: PETFINDER.shelter(animal.shelter_id).zip).name
-      animal.save
-    end
-  end
-
-  # def create
-
-  def create
-    # binding.pry
-    #redirect to results
-  end
-
-  #   Pet.destroy_all
-  #    @pets = PETFINDER.find_pets(pet_finder_type, pet_finder_zip, count: 500)
-  #    if params['pets']['breed'].empty?
-  #     @selected_animals = @pets.select do |pet|
-  #       pet.age == params['pets']['age']  &&
-  #       pet.size == params['pets']['size'] &&
-  #       pet.sex == params['pets']['sex']
-  #     end
-  #   else
-  #     @selected_animals = @pets.select do |pet|
-  #       pet.age == params['pets']['age']  &&
-  #       pet.size == params['pets']['size'] &&
-  #       pet.sex == params['pets']['sex'] &&
-  #       pet.breeds.include?(params['pets']['breeds'])
-  #     end
-  #   end
-
 
   def clean_params
     # binding.pry
     thinned_params.permit(:species, :zip_code, :sex, :size, :breed, :age)
   end
-
 
 end
